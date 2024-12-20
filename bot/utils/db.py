b4594a760b26777621539580bdfc7ffb3e175978
Peyman <itispey@gmail.com>
2024-12-20 22:59:27 +0330

    Add db.py

---
+++ bot/utils/db.py
@@ -0,0 +1,13 @@
+from config import Config
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    Config.DATABASE_URL,
+    pool_pre_ping=Config.POOL_PRE_PING,
+    pool_recycle=Config.POOL_RECYCLE
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
